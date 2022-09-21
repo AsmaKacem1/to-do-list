fd9ecea9c42ef0a1d39957a7725e63dc606a845a
Asma Kacem <asma.kacem217@gmail.com>
2022-09-21 09:45:05 +0100

    add initial database with mongoose

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose=require("mongoose");
 const app =express();
 
 app.use(express.static("public"));
 
-const items=[];
-const workItems= [];
+mongoose.connect("mongodb://localhost:27017/todolistDB",{UseNewUrlParser:true});
+
+const ItemsSchema ={
+    name:String
+};
+
+const Item=mongoose.model("item",ItemsSchema);
+
+const item1=new Item({
+    name:"welcome todolist site"
+});
+
+const item2=new Item({
+    name:"Hit the + button to add new item"
+});
+
+const item3=new Item({
+    name:"<-- Hit this to delete item"
+});
+
+const listItems=[item1,item2,item3];
+
+Item.insertMany(listItems,function(err){
+    if(err){
+        console.log("check there is an error!");
+    }else{
+        console.log("success!");
+    }
+})
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
